7837feefad40c2df1d84d614f5baa53a1fa2b6b2
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-23 20:09:09 -0800

    Updating eval stub

--- core/eval.py
+++ core/eval.py
@@ -6,9 +6,12 @@ Basically, eval() allows you to evaluate, or run Python code in a Python program
 or just unnecessary, it provides control flow for the majority of the interpreter. It is in the eval function 
 in which most of the logic for the interpreter is done.
 
+The eval function takes two parameters, the expression to be evaluated, as well as the environment in which
+the evaluation is done.
+
 Here is the control flow in the eval function:
 	1. First check is if the expression is a Symbol (variable) - In Python this just checks if the expression
-	is a string
+	is a string. The symbol is then found 
 
 	2. Second check is the constant check. If the expression is not a string, or it's not a list, than it's most
 	likely a numerical constant.
@@ -16,4 +19,4 @@ Here is the control flow in the eval function:
 	3. Third check is for the quote special form in Scheme. The interpreter supports functionality for both forms 
 	of quote, "quote" and '.
 
-	4. Fourt
+	4. Fourth check is for the 
