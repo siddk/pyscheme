3eed2b0ef7c9e3e6ac20c8a8c1656e6020c24278
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-23 16:36:43 -0800

    Adding stub to pyscheme.py

--- pyscheme.py
+++ pyscheme.py
@@ -1,2 +1,9 @@
+"""
+pyscheme.py
+
+Main file for the Scheme interpreter. Houses the main function, initializers, and imports the 
+Environment, Parse, and Eval functionality
+"""
+
 __author__ = "Sidd Karamcheti"
 
