37800624dd1d75d43e92145fe8b41e0b3b4e72ec
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-24 23:26:46 -0800

    Refactoring

--- core/eval.py
+++ core/eval.py
@@ -1,26 +1,33 @@
 """
 eval.py
 
-The eval() function is this really cool, but under-used function built into the Python programming language.
-Basically, eval() allows you to evaluate, or run Python code in a Python program. While this may seem redundant,
-or just unnecessary, it provides control flow for the majority of the interpreter. It is in the eval function 
+The eval() function is this really cool, but under-used function built into
+the Python programming language.
+
+Basically, eval() allows you to evaluate, or run Python code in a Python 
+program. While this may seem redundant, or just unnecessary, it provides 
+control flow for the majority of the interpreter. It is in the eval function 
 in which most of the logic for the interpreter is done.
 
-The eval function takes two parameters, the expression to be evaluated, as well as the environment in which
-the evaluation is done.
+The eval function takes two parameters, the expression to be evaluated, as 
+well as the environment in which the evaluation is done.
 
 Here is the control flow in the eval function:
-	1. First check is if the expression is a Symbol (variable) - In Python this just checks if the expression
-	is a string. The symbol is then found in the environment, and returned.
+    1. First check is if the expression is a Symbol (variable) - In Python 
+    this just checks if the expression is a string. The symbol is then found 
+    in the environment, and returned.
 
-	2. Second check is the constant check. If the expression is not a string, or it's not a list, than it's most
-	likely a numerical constant. Returns the constant value.
+    2. Second check is the constant check. If the expression is not a string, 
+    or it's not a list, than it's most likely a numerical constant. Returns the constant value.
 
-	3. Third check is for the quote special form in Scheme. The interpreter supports functionality for both forms 
-	of quote, "quote" and '. Returns the expression as it is.
+    3. Third check is for the quote special form in Scheme. The interpreter 
+    supports functionality for both forms of quote, "quote" and '. Returns the
+    expression as it is.
 
-	4. Fourth check is for the "if" special form. The expression is then tokenized, and separated into three parts:
-	the conditional (test), and the conditional statements, consequence and alternative, respectively. The condition 
-	is then evaluated, and the resulting term is then run through the eval() function again.
+    4. Fourth check is for the "if" special form. The expression is then
+    tokenized, and separated into three parts: the conditional (test), and the
+    conditional statements, consequence and alternative, respectively. The
+    condition is then evaluated, and the resulting term is then run through 
+    the eval() function again.
 
-	5. Fifth check is for the 
+    5. Fifth check is for the 
