853a6e9a7b40ee5af41b20cf1ef9d703ae222425
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-24 23:19:19 -0800

    Refactoring

--- pyscheme.py
+++ pyscheme.py
@@ -1,8 +1,8 @@
 """
 pyscheme.py
 
-Main file for the Scheme interpreter. Houses the main function, initializers, and imports the 
-Environment, Parse, and Eval functionality
+Main file for the Scheme interpreter. Houses the main function, initializers, 
+and imports the Environment, Parse, and Eval functionality
 """
 
 __author__ = "Sidd Karamcheti"
