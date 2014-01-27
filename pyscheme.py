3f1a25821dcdbab1c8e845911d4a224b513fb106
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-27 10:13:55 -0800

    Adding REPL

--- pyscheme.py
+++ pyscheme.py
@@ -8,8 +8,19 @@ and imports the Environment, Parse, and Eval functionality
 __author__ = "Sidd Karamcheti"
 
 from core.environment import *
-from core.eval import *
-from core.parser import parse
+from core.eval import eval as evaluate
+from core.parser import parse, toString
+
+
+def repl(prompt='pyscheme>> '):
+    """
+    Live, interactive REPL for pyscheme.
+    """
+    while True:
+        val = evaluate(parse(raw_input(prompt)))
+        if val is not None:
+            print toString(val)
+
 
 if __name__ == "__main__":
-
+    repl()
