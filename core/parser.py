35d9dd768a7be8c4b0c07867204ce4f6e9ccd02e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-27 09:49:44 -0800

    Adding tokenize function, with docstring

--- core/parser.py
+++ core/parser.py
@@ -8,5 +8,17 @@ __author__ = "Sidd Karamcheti"
 
 
 def parse(scheme_expression):
-    "Calls the read() function on the tokenized expression."
+    """
+    Calls the read() function on the tokenized expression.
+    """
     return read(tokenize(scheme_expression))
+
+def tokenize(expression):
+    """
+    Inserts spaces between parentheses in a scheme expression, then splits the expression to keep track of
+    all sub-expressions
+    """
+    expression = expression.replace('(', ' ( ')
+    expression = expression.replace(')', ' ) ')
+
+    return expression.split()
