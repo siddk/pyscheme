34d082251ee746758a92afc1d47cb3f6850acc48
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-27 09:46:58 -0800

    Adding parse main function

--- core/parser.py
+++ core/parser.py
@@ -6,3 +6,7 @@ Parses out Scheme expressions from input. Takes a string, tokenizes it, creates 
 
 __author__ = "Sidd Karamcheti"
 
+
+def parse(scheme_expression):
+    "Calls the read() function on the tokenized expression."
+    return read(tokenize(scheme_expression))
